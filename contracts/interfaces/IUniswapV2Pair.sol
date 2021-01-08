8fd71868192b425e34bd954d08452eacbdfd3a61
Xiahong Lin <1943.com@gmail.com>
2021-01-08 14:15:40 -0800

    Add initial flashloan liquidity logic.

--- contracts/interfaces/IUniswapV2Pair.sol
+++ contracts/interfaces/IUniswapV2Pair.sol
@@ -21,32 +21,20 @@ interface IUniswapV2Pair {
 
     function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
 
-    event Mint(address indexed sender, uint amount0, uint amount1);
-    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
-    event Swap(
-        address indexed sender,
-        uint amount0In,
-        uint amount1In,
-        uint amount0Out,
-        uint amount1Out,
-        address indexed to
-    );
-    event Sync(uint112 reserve0, uint112 reserve1);
+    event Mint(address indexed sender, uint amount);
+    event Burn(address indexed sender, uint amount, address indexed to);
+    event Sync(uint112 reserve);
 
     function MINIMUM_LIQUIDITY() external pure returns (uint);
     function factory() external view returns (address);
-    function token0() external view returns (address);
-    function token1() external view returns (address);
-    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
-    function price0CumulativeLast() external view returns (uint);
-    function price1CumulativeLast() external view returns (uint);
+    function token() external view returns (address);
+    function getReserves() external view returns (uint112 reserve);
     function kLast() external view returns (uint);
 
     function mint(address to) external returns (uint liquidity);
-    function burn(address to) external returns (uint amount0, uint amount1);
-    function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
+    function burn(address to) external returns (uint amount);
     function skim(address to) external;
     function sync() external;
 
-    function initialize(address, address) external;
+    function initialize(address) external;
 }
